417391e932bb85772cbd1b92d2be1f242312b550
mdshi <mikeshifera@gmail.com>
2018-07-28 10:21:32 -0500

    first commit

---
+++ src/main/java/VersioningDemo.java
@@ -0,0 +1,8 @@
+public class VersioningDemo {
+    public static void main (String args0[]) {
+        System.out.println ( "Hello World!" );
+        System.out.println ();
+        System.out.println ( "This program is running version " +
+        VersioningDemo.class.getCanonicalName ());
+    }
+}
